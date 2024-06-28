54a6ef6f68c4e39ae45dd935b92cb7cd70a62e14
tedschu <ted.schuster@gmail.com>
2024-06-28 09:51:45 -0500

    finished seed file

--- server/db/seed.js
+++ server/db/seed.js
@@ -49,6 +49,52 @@ async function main() {
       user_id: testUser.id,
     },
   });
+
+  // CREATING SECOND USER
+  const secondUser = await prisma.user.create({
+    data: {
+      name: "Jimmy",
+      birth_year: 2016,
+      email: "test@email.com",
+      username: "Jimmy",
+      password: "test",
+    },
+  });
+
+  await prisma.score.create({
+    data: {
+      addition_score: 780,
+      subtraction_score: 1200,
+      division_score: 300,
+      multiplication_score: 975,
+      user_id: secondUser.id,
+    },
+  });
+
+  await prisma.badge.create({
+    data: {
+      dog: true,
+      hippo: true,
+      shield_dog: true,
+      dove: true,
+      user_id: secondUser.id,
+    },
+  });
+
+  await prisma.user_stats.create({
+    data: {
+      total_logins: 7,
+      last_login: now,
+      user_id: secondUser.id,
+    },
+  });
+
+  await prisma.game_state.create({
+    data: {
+      json_setting: "difficulty: 5",
+      user_id: secondUser.id,
+    },
+  });
 }
 
 console.log(now);
