2e089261b9f6d45fd5a1919f0afaae1bc4e8f376
tedschu <ted.schuster@gmail.com>
2024-07-01 19:15:05 -0500

    adding initial frontend structure files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,23 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import { Route, Router, Routes } from "react-router-dom";
+import React from "react";
+
+import GamePlay from "./components/GamePlay";
+import Nav from "./components/Nav";
+import ScoreBar from "./components/ScoreBar";
+import Slider from "./components/Slider";
+import Game from "./pages/Game";
+import Me from "./pages/Me";
+import Register from "./pages/Register";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  <Nav />;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Welcome to WoofMath.</div>
     </>
   );
 }
---
+++ client/src/components/GamePlay.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function GamePlay() {
+  return <></>;
+}
+
+export default GamePlay;
