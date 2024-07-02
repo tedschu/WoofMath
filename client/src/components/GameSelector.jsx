e665fe25cea2c03b18c9d4a3d156233fbd207f33
tedschu <ted.schuster@gmail.com>
2024-07-02 10:51:23 -0500

    adding gameselector component

---
+++ client/src/components/GameSelector.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function GameSelector() {
+  return (
+    <>
+      {/* Buttons for selecting math problems */}
+
+      <div className="gameSelectContainer">
+        <h3>What kind of math do you want to play?</h3>
+        <div className="gameSelectButtons">
+          <button>Addition (+)</button>
+          <button>Subtraction (-)</button>
+          <button>Multiplication (*)</button>
+          <button>Division (%)</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GameSelector;
--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -5,23 +5,14 @@ import { useNavigate } from "react-router-dom";
 import ScoreBar from "../components/ScoreBar";
 import Slider from "../components/Slider";
 import GamePlay from "../components/GamePlay";
+import GameSelector from "../components/GameSelector";
 
 function Game() {
   return (
     <>
       <ScoreBar />
 
-      {/* Buttons for selecting math problems */}
-
-      <div className="gameSelectContainer">
-        <h3>What kind of math do you want to play?</h3>
-        <div className="gameSelectButtons">
-          <button>Addition (+)</button>
-          <button>Subtraction (-)</button>
-          <button>Multiplication (*)</button>
-          <button>Division (%)</button>
-        </div>
-      </div>
+      <GameSelector />
 
       <Slider />
 
