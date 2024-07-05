9436a2a980499c25ba62524e558985fa81f7e935
tedschu <ted.schuster@gmail.com>
2024-07-04 23:04:14 -0500

    added initial login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,7 +4,42 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <>Login page</>;
+  return (
+    <>
+      <div className="loginPageContainer">
+        <div className="loginContainer">
+          <h2>Hey, you!</h2>
+          <p>
+            Welcome to WoofMath, a game where you can practice math and earn
+            points + super cool animal badges as you go along. The more math you
+            do, the more badges you get!
+          </p>
+
+          <p>Login to begin.</p>
+
+          <div className="loginInputs">
+            <input
+              type="text"
+              placeholder="Username..."
+              // value={userAnswer}
+              // onChange={setAnswer}
+            />
+            <input
+              type="password"
+              placeholder="Password..."
+              // value={userAnswer}
+              // onChange={setAnswer}
+            />
+            <button>Log in</button>
+          </div>
+          <h4>
+            Wait...I don't have an account! No worries,{" "}
+            <Link to={"/register"}>create a free account here. </Link>
+          </h4>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
