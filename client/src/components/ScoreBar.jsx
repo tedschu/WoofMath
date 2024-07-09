ee93537ee4118e8a3bbf653e28b92aeeeba0da76
tedschu <ted.schuster@gmail.com>
2024-07-09 09:24:48 -0500

    updated CSS for scorebar

--- client/src/components/ScoreBar.jsx
+++ client/src/components/ScoreBar.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHippo } from "@fortawesome/free-solid-svg-icons";
+import { faFrog } from "@fortawesome/free-solid-svg-icons";
+import { faDove } from "@fortawesome/free-solid-svg-icons";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
 
 function ScoreBar({
   isLoggedIn,
@@ -36,9 +41,29 @@ function ScoreBar({
   return (
     <>
       <div className="scoreBarContainer">
-        <div className="scores">Score is {totalScore}</div>
+        <div className="scores">
+          <div className="totalScores">
+            <h3>Your total score is {totalScore}</h3>
+          </div>
+          <div className="individualScores">
+            Addition: 474 | Subtraction: 23 | Multiplication: 0 | Division: 0
+          </div>
+        </div>
 
-        <div className="badges">Badges</div>
+        <div className="badges">
+          <FontAwesomeIcon icon={faHippo} size={"2x"} className="badgeEnabled">
+            {" "}
+          </FontAwesomeIcon>
+          <FontAwesomeIcon icon={faFrog} size={"2x"} className="badgeEnabled">
+            {" "}
+          </FontAwesomeIcon>
+          <FontAwesomeIcon icon={faDove} size={"2x"} className="badgeDisabled">
+            {" "}
+          </FontAwesomeIcon>
+          <FontAwesomeIcon icon={faCat} size={"2x"} className="badgeDisabled">
+            {" "}
+          </FontAwesomeIcon>
+        </div>
       </div>
     </>
   );
